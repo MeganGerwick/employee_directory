ec3aec7f001b25ec20b364d4eed012ef65e28f81
Louisa May <mgerwick@gmail.com>
2021-02-08 16:29:44 -0600

    created header component

--- user_directory/src/components/header.js
+++ user_directory/src/components/header.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./style.css";
+import { Jumbotron, Row, Col, Container } from 'reactstrap'
 
-function Header() {
+export default function Header(props) {
     return (
-        <div className="hero text-center" style={{ backgroundImage: '' }}>
+        <div>
+            <Jumbotron fluid>
+                <Container fluid>
+                    <Row>
+                        <Col sm="12" md={{ size: 6, offset: 3 }}>
+                            <h1 className="display-3 text-warning">Employee Directory</h1>
+                            <h5 className="text-warning"> Use the arrows to filter in ascending or descending alphabetical order</h5>
+                        </Col>
+                    </Row>
+                </Container>
+            </Jumbotron>
         </div>
     );
 }
 
-export default Header;
--- user_directory/src/components/wrapper.js
+++ user_directory/src/components/wrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Wrapper(props) {
     return <main className={`wrapper${props.fluid ? '-fluid' : ''}`}>
-        {props.children}
+        g{props.children}
     </main>;
 }
 
