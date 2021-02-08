7c825ba6e0cf9730207ae5c8930efcb9240c87a0
Louisa May <mgerwick@gmail.com>
2021-02-08 16:05:08 -0600

    Wrapper component created

--- user_directory/src/components/wrapper.js
+++ user_directory/src/components/wrapper.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Wrapper(props) {
-    return <main className="wrapper">{props.children}</main>;
+    return <main className={`wrapper${props.fluid ? '-fluid' : ''}`}>
+        {props.children}
+    </main>;
 }
 
 export default Wrapper;
