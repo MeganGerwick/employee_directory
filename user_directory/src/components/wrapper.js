7c7a0ce7150c651ca394756d333792ef38648003
Louisa May <mgerwick@gmail.com>
2021-02-08 18:33:51 -0600

    fixed folder

--- user_directory/src/components/wrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Wrapper(props) {
-    return <main className={`wrapper${props.fluid ? '-fluid' : ''}`}>
-        g{props.children}
-    </main>;
-}
-
-export default Wrapper;
